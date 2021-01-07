336e9315fb1cd241e37d86cf69b50ec4a535610b
sorenmulli <swholm@protonmail.com>
2021-01-07 18:44:32 +0100

    Make home path more general

--- zsh/home_path.sh
+++ zsh/home_path.sh
@@ -1,1 +1,1 @@
-export PATH=/home/sorenwh/.local/bin:/home/sorenwh/maple2020/bin:/home/sorenwh/installs/maple2020/bin:/usr/local/maple2020/bin/:$PATH
+export PATH=$HOME/.local/bin:$HOME/maple2020/bin:$HOME/installs/maple2020/bin:/usr/local/maple2020/bin/:$PATH
